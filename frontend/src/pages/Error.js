18ee75282c76f5bb7729d4ab9cc45049f9d6e9f3
kush90 <kushgc90@gmail.com>
2024-04-06 16:56:21 +0700

    updated error page for route not found

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 function Error() {
+  const goBack = () => {
+    window.history.back();
+  };
+
   return (
-    <div>Error</div>
-  )
+    <div id='error' style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+      <h1 className="notFoundTitle">Oops! That page can’t be found.</h1>
+      <p className="notFoundDesc">
+        It looks like nothing was found at this location.
+        Please click  <a  href="#" onClick={goBack} size='sm'>Back</a>  to go to the previous page.
+      </p>
+    </div>
+  );
 }
 
-export default Error
-
+export default Error;
