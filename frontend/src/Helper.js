ec50ce49eea85d1de808de1199f74d2ec7560b55
kush90 <kushgc90@gmail.com>
2024-12-25 12:00:14 +0700

    added domain api

--- frontend/src/Helper.js
+++ frontend/src/Helper.js
@@ -1,6 +1,8 @@
 // export const API_URL = "http://localhost:4000";
 // export const API_URL = "https://diamond-api-50e4.onrender.com";
-export const API_URL = "http://52.63.48.17:4000";
+// export const API_URL = "http://52.63.48.17:4000";
+export const API_URL = "https://api.businessalliancehub.org";
+
 
 export const createStorage = (key, item) => {
     if (!localStorage.getItem(key)) localStorage.setItem(key, JSON.stringify(item));
